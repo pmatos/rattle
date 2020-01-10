40e3e4958e994d8c3e976eaba091a125dc9db981
Paulo Matos <pmatos@linki.tools>
2020-01-10 18:05:02 +0100

    Initial commit

---
+++ src/runtime.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* define all scheme constants */
+#define bool_f 0x2f
+#define bool_t 0x6f
+#define fx_mask 0x03
+#define fx_tag 0x00
+#define fx_shift 2
+
+/* all scheme values are of type ptrs */
+typedef unsigned int ptr;
+
+static void
+print_ptr(ptr x)
+{
+  if ((x & fx_mask) == fx_tag)
+    printf("%d", ((int) x) >> fx_shift);
+  else if(x == bool_f)
+    printf("#f");
+  else if(x == bool_t)
+    printf("#t");
+  else
+    printf("#<unknown 0x%08x>", x);
+  printf("\n");
+}
+
+int
+main(int argc, char **argv)
+{
+  print_ptr(scheme_entry());
+  return 0;
+}
